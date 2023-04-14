d93cae5f24077dd160fb4336839f162a90f22aa6
yafidaffaa <yafi.d.andriansyah@gmail.com>
2023-04-14 14:46:12 +0700

    membuat sebuah kondisi dimana jumlah mainumim array 1 dan jumlah maksimum 20

--- linearSearch/linearSearch.cpp
+++ linearSearch/linearSearch.cpp
@@ -11,6 +11,9 @@ void input()
 	{
 		cout << "Enter the number og=f elements in the array: ";
 		cin >> n;
-		
+		if ((n > 0) && (n <= 20))
+			break;
+		else
+			cout << "\nArray should have minimum 1 and maximum 20 elements.\n\n";
 	}
 }
