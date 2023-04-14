5cc5b258a21f14d409c61888277531b5ba76e9dd
yafidaffaa <yafi.d.andriansyah@gmail.com>
2023-04-14 14:50:50 +0700

    membuat perulangan untuk memasukan data dari array

--- linearSearch/linearSearch.cpp
+++ linearSearch/linearSearch.cpp
@@ -16,4 +16,14 @@ void input()
 		else
 			cout << "\nArray should have minimum 1 and maximum 20 elements.\n\n";
 	}
+
+	//Accept array elements
+	cout << "\n----------------------\n";
+	cout << " Enter array elements \n";
+	cout << "----------------------\n";
+	for (i = 0; i < n; i++)
+	{
+		cout << "<" << (i + 1) << "> ";
+		cin >> arr[i];
+	}
 }
